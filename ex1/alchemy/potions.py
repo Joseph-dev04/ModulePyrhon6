0cbfc4b343e529852c802486bd159ac8c6f21b8c
Joseph-dev04 <jpajuelo.dev@gmail.com>
2026-03-26 15:54:04 +0100

    20 %

---
+++ ex1/alchemy/potions.py
@@ -0,0 +1,14 @@
+def healing_potion() -> None:
+    return "Healing potion brewed with [fire_result] and [water_result]"
+
+
+def strength_potion() -> None:
+    return "Strength potion brewed with [earth_result] and [fire_result]"
+
+
+def invisibility_potion() -> None:
+    return "Invisibility potion brewed with [air_result] and [water_result]"
+
+
+def wisdom_potion() -> None:
+    return "Wisdom poriton brewed with all elements: [all_for_results]"
---
+++ ex1/ft_import_transmutation.py
@@ -0,0 +1,6 @@
+import alchemy
+import alchemy.elements
+
+
+if __name__ == "__main__":
+	
